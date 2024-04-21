1ff633ad2529b7d8260c24de90af73aea87cb85d
Benkunde Vijaynand Dhanraj <140649943+Vijaynand2535@users.noreply.github.com>
2024-04-21 15:10:53 +0530

    work on tdform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'machinetestTDform';
+  @ViewChild ("signup") signup!: NgForm
+  mocArr = [
+  {contactMode : 'Email', id : 123},
+  {contactMode : 'Phone', id : 124}
+  ]
+  userdetails = {
+  "userInfo": { 
+    "username": "vijaybenkunde", 
+    "email": "vijay@gmail.com" 
+  }, 
+    "secretquestions": "favouritebook", 
+    "answer": "my book", 
+    "isSubscribe": true, 
+    "modeofcontact": "Email" 
+
+  }
+  constructor(){
+
+  }
+ngOnInit(): void {
+  
 }
+  onSignUp(signup : NgForm){
+    
+    // if(signup.valid){
+    //   // console.log(signup.valid);
+    //   // console.log(signup);
+    //   console.log(signup.value);
+      
+    // }
+    console.log(signup.value);
+  }
+  onEdit(){
+    this.signup.form.patchValue(this.userdetails)
+  }
+}
+
